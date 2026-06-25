fe2011ffe78d3d8226e81fb74f0d560d7d2d9d6d
QiuShui1012 <qiushui1012@foxmail.com>
2026-06-26 07:48:59 +0800

    fix(ClientTickRecorder): simplify EventBusSubscriber annotation by removing modid

--- module.util/src/main/java/dev/anvilcraft/lib/v2/util/ClientTickRecorder.java
+++ module.util/src/main/java/dev/anvilcraft/lib/v2/util/ClientTickRecorder.java
@@ -8,7 +8,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import org.jetbrains.annotations.ApiStatus;
 
-@EventBusSubscriber(modid = AnvilLibUtil.MAIN_ID, value = Dist.CLIENT)
+@EventBusSubscriber(Dist.CLIENT)
 public class ClientTickRecorder {
     @Getter
     private static int ticks;
