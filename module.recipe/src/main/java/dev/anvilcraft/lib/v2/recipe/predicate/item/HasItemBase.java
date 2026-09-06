f37471b7806b2b97b5188de1e9381cd6d3421b4c
PopSlime <61138090+PopSlime@users.noreply.github.com>
2026-09-06 14:23:06 +0800

    fix: 修复对配方物品执行 SaveComponentToTag 谓词函数时发生崩溃的问题 (#112)

--- module.recipe/src/main/java/dev/anvilcraft/lib/v2/recipe/predicate/item/HasItemBase.java
+++ module.recipe/src/main/java/dev/anvilcraft/lib/v2/recipe/predicate/item/HasItemBase.java
@@ -6,16 +6,16 @@ import dev.anvilcraft.lib.v2.codec.StreamCodecUtil;
 import dev.anvilcraft.lib.v2.recipe.AnvilLibRecipe;
 import dev.anvilcraft.lib.v2.recipe.cache.ItemCache;
 import dev.anvilcraft.lib.v2.recipe.cache.item.ICacheInput;
-import dev.anvilcraft.lib.v2.util.predicate.IItemStackPredicate;
 import dev.anvilcraft.lib.v2.recipe.predicate.IRecipePredicate;
 import dev.anvilcraft.lib.v2.recipe.predicate.function.IPredicateFunction;
 import dev.anvilcraft.lib.v2.recipe.util.InWorldRecipeContext;
 import dev.anvilcraft.lib.v2.recipe.util.InWorldRecipeData;
+import dev.anvilcraft.lib.v2.util.predicate.IItemStackPredicate;
 import lombok.Getter;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
@@ -76,8 +76,9 @@ public abstract class HasItemBase<T extends HasItemBase<T, P>, P extends IItemSt
         ICacheInput item1 = this.getItem(context);
         item1.apply(itemStack -> {
             for (IPredicateFunction<?> function : this.functions) {
-                IPredicateFunction<ItemStack> function1 = (IPredicateFunction<ItemStack>) function;
-                itemStack = function1.apply(context, itemStack);
+                // TODO: This cast is unsafe. Refactor in the future?
+                IPredicateFunction<ItemStackTemplate> function1 = (IPredicateFunction<ItemStackTemplate>) function;
+                itemStack = function1.apply(context, ItemStackTemplate.fromNonEmptyStack(itemStack)).create();
             }
         });
     }
